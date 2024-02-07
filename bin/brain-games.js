46b5085a33715ecc5991ec4cfae06eceff8335f1
vasilii <vasilii@DESKTOP-PLEIMQ1>
2024-02-07 11:58:00 +0300

    ada

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!')
+console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('Your answer: ');
+
+console.log('Hello,'+name);
