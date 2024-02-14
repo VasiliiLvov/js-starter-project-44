dbe9bd3df1e88eb19be5be7384175bcf90cd753a
vasilii <vasilii@DESKTOP-PLEIMQ1>
2024-02-14 12:13:05 +0300

    aaa

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
@@ -7,30 +8,34 @@ const name = readlineSync.question('Your answer: ');
 
 console.log(`Hello,${name}`);
 console.log('Find the greatest common divisor of given numbers.');
-let l = Math.floor(Math.random() * 100);
-const k = Math.floor(Math.random() * 100);
-console.log('Question:' + ` ${l} ${k}`);
-let j = 0;
-let h = k;
-if (l > k) {
-  while ((l % k) !== 0 && k % h !== 0) {
-    h -= 1;
+let game=0;
+while(game<3){
+let num1 = Math.floor(1+Math.random() * 100);
+let num2 = Math.floor(1+Math.random() * 100);
+console.log('Question:' + ` ${num1} ${num2}`);
+let nod=0;
+  if(num2>num1){
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
   }
-  j = h;
-  console.log(j);
-} else if (l < k) {
-  while (k % l !== 0 && k % h !== 0) {
-    l -= 1;
+  for (let i = num2; i > 0; i -= 1){
+    if (num1%i === 0 && num2%i === 0){
+          nod = i;
+        break;
+    }
   }
-  j = h;
-  console.log(j);
-} else if (l === k) {
-  j = l;
-  console.log(j);
-}
 const gcd = readlineSync.question('Your answer: ');
-if (gcd === j) {
+if (Number(gcd) === nod) {
   console.log('Correct!');
+  game +=1;
 } else {
   console.log('Wrong!');
+  break;
+}
+}
+if (game<3){
+  console.log('Try again,  '+ name)
+} else{
+  console.log('Congratulations,'+name)
 }
