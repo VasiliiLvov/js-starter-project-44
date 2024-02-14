8588361d5c400862bd7db8e27dbc6a0ba20524f4
vasilii <vasilii@DESKTOP-PLEIMQ1>
2024-02-14 15:11:41 +0300

    aaa

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -8,30 +8,30 @@ const name = readlineSync.question('Your answer: ');
 
 console.log(`Hello,${name}`);
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-let p=0;
-while(p<3){
-let num = 2 + Math.floor(Math.random() * 100);
-let divMeter = 0;
-for(let i =2;i<num;i++){
-    if (num%i === 0){
-        divMeter += 1;
+let p = 0;
+while (p < 3) {
+  const num = 2 + Math.floor(Math.random() * 100);
+  let divMeter = 0;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      divMeter += 1;
     }
-}
-console.log(`Question:${num}`);
-const answer = readlineSync.question('Your answer: ');
-if (answer === 'yes' && divMeter === 0){
+  }
+  console.log(`Question:${num}`);
+  const answer = readlineSync.question('Your answer: ');
+  if (answer === 'yes' && divMeter === 0) {
     console.log('Correct!');
-    p +=1;
-} else if (answer === 'no' && divMeter > 0){
+    p += 1;
+  } else if (answer === 'no' && divMeter > 0) {
     console.log('Correct!');
-    p +=1;
-} else{
+    p += 1;
+  } else {
     console.log('Wrong!');
     break;
-}
- }
- if (p < 3) {
-    console.log(`Try again,  ${name}`);
-  } else {
-    console.log(`Congratulations,${name}`);
   }
+}
+if (p < 3) {
+  console.log(`Try again,  ${name}`);
+} else {
+  console.log(`Congratulations,${name}`);
+}
