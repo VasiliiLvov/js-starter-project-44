8e66bcdb9b21deaef004315dac5d3b261c098fc3
vasilii <vasilii@DESKTOP-PLEIMQ1>
2024-02-25 14:09:54 +0300

    111

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 
-
+import readlineSync from 'readline-sync';
 export default() => {
 console.log('Welcome to the Brain Games!');
 console.log('May I have your name? ');
--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import printhello from '/home/vasilii/js-starter-project-44/src/cli.js';
-printhello();
+console.log('Welcome to the Brain Games!');
+console.log('May I have your name? ');
 var names = readlineSync.question('Your answer: ');
 console.log(`Hello, ${names}`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
 let p = 0;
 while (p < 3) {
   p += 1;
