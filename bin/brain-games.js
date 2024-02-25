66f949a62a574e720971e83f5239803745b78bff
vasilii <vasilii@DESKTOP-PLEIMQ1>
2024-02-25 14:06:45 +0300

    111

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,3 @@
 #!/usr/bin/env node
 import printhello from '../src/cli.js';
 printhello();
-var names = readlineSync.question('Your answer: ');
-console.log(`Hello, ${names}`);
--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,6 @@
 export default() => {
 console.log('Welcome to the Brain Games!');
 console.log('May I have your name? ');
+var names = readlineSync.question('Your answer: ');
+console.log(`Hello, ${names}`);
 };
