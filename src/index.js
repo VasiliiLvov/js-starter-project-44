8f866ae894ffcf01be477661133cbf58691f87d1
vasilii <vasilii@DESKTOP-PLEIMQ1>
2024-03-06 13:11:38 +0300

    a

--- src/index.js
+++ src/index.js
@@ -16,19 +16,19 @@ const startBrainGames = (question, calculator) => {
         console.log('Correct!');
       } else {
         console.log(`'${answer}' is wrong answer ;(. Correct answer was '${Rightanswer}'.`);
-        console.log(`Let's try again, ${names}!`)
+        console.log(`Let's try again, ${names}!`);
         break;
       }
     } else if (Number(answer) === Rightanswer) {
       console.log('Correct!');
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${Rightanswer}'.`);
-      console.log(`Let's try again, ${names}!`)
+      console.log(`Let's try again, ${names}!`);
       break;
     }
     if (p > 2) {
       console.log(`Congratulations, ${names}!`);
-    } 
+    }
   }
 };
 export default startBrainGames;
